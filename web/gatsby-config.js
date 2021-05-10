4dc56e418e113c0c5a9d3bbc45d5eca7ea77b72f
weronikadominiak <weronika.dominiak@hotmail.com>
2021-05-10 13:13:29 +0100

    Move studio to separate folder, add gatsby

---
+++ web/gatsby-config.js
@@ -0,0 +1,25 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: `yjb5bfaw`,
+        dataset: "production",
+        // a token with read permissions is required
+        // if you have a private dataset
+        token: process.env.SANITY_TOKEN,
+
+        // If the Sanity GraphQL API was deployed using `--tag <name>`,
+        // use `graphqlTag` to specify the tag name. Defaults to `default`.
+        graphqlTag: "default",
+      },
+    },
+  ],
+}
