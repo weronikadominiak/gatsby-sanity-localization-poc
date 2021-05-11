a33dd39af575c1007ae18573795ede7dcaf3c570
weronikadominiak <weronika.dominiak@hotmail.com>
2021-05-11 20:27:04 +0100

    Studio | Custom isUnique for slugs - ensure slugs are unique within market, allow same slugs for different markets

--- studio/schemas/person.js
+++ studio/schemas/person.js
@@ -1,5 +1,25 @@
 import UserIcon from "part:@sanity/base/user-icon";
 
+import client from "part:@sanity/base/client";
+
+// Note: this assumes that every document that has a slug field
+// have it on the `slug` field at the root
+export function isUniqueAcrossAllDocuments(slug, options) {
+  const { document } = options;
+
+  const id = document._id.replace(/^drafts\./, "");
+  const params = {
+    draft: `drafts.${id}`,
+    published: id,
+    slug,
+    market: document.market,
+  };
+
+  const query = `!defined(*[!(_id in [$draft, $published]) && slug.current == $slug && market == $market][0]._id)`;
+
+  return client.fetch(query, params);
+}
+
 export default {
   name: "person",
   title: "Person",
@@ -26,6 +46,7 @@ export default {
       type: "slug",
       options: {
         source: "name.base.name",
+        isUnique: isUniqueAcrossAllDocuments,
         maxLength: 96,
       },
     },
